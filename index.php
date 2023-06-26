0cd0b70191b4c65f289f7083f68cf9c34d85cfce
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2023-06-27 00:22:35 +1000

    Established dbase connection and fetched all records

--- index.php
+++ index.php
@@ -1,13 +1,31 @@
 <?php
-    // Create constants to store the database credentials
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'crys');
-    define('DB_PASS', 'crys3000');
-    define('DB_NAME', 'pdo-example');
+    /* Establish database connection */
 
-    // Establish new database connection
+    // Create constants to store the database credentials
+    $host = 'localhost';
+    $user = 'crys';
+    $password = 'crys3000';
+    $dbname = 'pdo-posts';
+
+    // Create DSN (Data Source Name)
+    $dsn = 'mysql:host=' . $host . ';dbname=' . $dbname;
+
+    // Create new PDO instance
+    $connection = new PDO($dsn, $user, $password);
     
-    // Check connection
+    /* Retrieve all records from database 'posts' table */
+    
+    // Create the query
+    $query = $connection->query('SELECT * FROM posts');
+
+    // Execute the query and assign the result to $posts
+    $posts = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    print_r($posts);
+
+    // while($row = $query->fetchAll(PDO::FETCH_OBJ)) {
+    //     echo $row['title'] . '<br>';
+    // }
 
 
 ?>
