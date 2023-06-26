89bd4b5ea696d687d87b32f73d9a688261b4d4f7
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2023-06-26 23:31:15 +1000

    Defined constants containing the dbase credentials

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    // Create constants to store the database credentials
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'crys');
+    define('DB_PASS', 'crys3000');
+    define('DB_NAME', 'pdo-example');
+
+    // Establish new database connection
+    
+    // Check connection
+
+
+?>
